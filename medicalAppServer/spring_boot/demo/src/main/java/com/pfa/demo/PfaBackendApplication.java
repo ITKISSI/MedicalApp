044133d4694963dc0efa74b2ea6a79739570f359
7afidi <dev7afidi@gmail.com>
2023-04-02 04:21:41 +0000

    spring boot project added

---
+++ medicalAppServer/spring_boot/demo/src/main/java/com/pfa/demo/PfaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pfa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfaBackendApplication.class, args);
+	}
+
+}
