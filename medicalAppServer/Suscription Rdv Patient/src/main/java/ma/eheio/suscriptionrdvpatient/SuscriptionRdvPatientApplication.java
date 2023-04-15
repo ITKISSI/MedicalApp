46ebd6db84b51304eb3bd0d6dd732b1a836c50ef
KISSI Abdelaziz <azizkissi29@gmail.com>
2023-04-15 14:09:57 +0000

    Dockerize app with pgadmin and postgres

---
+++ medicalAppServer/Suscription Rdv Patient/src/main/java/ma/eheio/suscriptionrdvpatient/SuscriptionRdvPatientApplication.java
@@ -0,0 +1,13 @@
+package ma.eheio.suscriptionrdvpatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuscriptionRdvPatientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuscriptionRdvPatientApplication.class, args);
+    }
+
+}
---
+++ medicalAppServer/Suscription Rdv Patient/src/test/java/ma/eheio/suscriptionrdvpatient/SuscriptionRdvPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.eheio.suscriptionrdvpatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuscriptionRdvPatientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
