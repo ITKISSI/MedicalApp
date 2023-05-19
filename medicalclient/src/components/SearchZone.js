d80e981bf9394f855756720968d22d95024dc07a
meryemmh <meryemmeharzi2001@gmail.com>
2023-05-19 04:03:49 +0100

    searchZone modification add ville filter

--- medicalclient/src/components/SearchZone.js
+++ medicalclient/src/components/SearchZone.js
@@ -1,16 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function SearchZone() {
+const SearchZone = () => {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
   const [filter, setFilter] = useState('All');
-
-
-  // useEffect(() => {
-  //   Geocode.setApiKey('AIzaSyC-2H5_BTMVElmn0c-sgYcqc1CEx7jrbCs');
-  // }, []);
-
-
+  const [selectedCity, setSelectedCity] = useState('');
+  const cities = ['Casablanca', 'Rabat', 'Marrakech', 'Oujda','Tata'];
 
   const handleSearch = async (event) => {
     event.preventDefault();
@@ -30,8 +25,7 @@ function SearchZone() {
         filteredResults = medecinData.medecinList.map((medecin) => ({ doctor: medecin }));
       } else if (filter === 'Clinic') {
         filteredResults = cabinetData.cabinetList.map((cabinet) => ({ cabinet }));
-      }
-      else if (filter === 'Specialite') {
+      } else if (filter === 'Specialite') {
         filteredResults = medecinData.medecinList.map((medecin) => ({ doctor: medecin, specialite: medecin.specialite }));
       }
     } else {
@@ -48,16 +42,57 @@ function SearchZone() {
           medecin.specialite.toLowerCase().includes(query.toLowerCase())
         ).map((medecin) => ({ doctor: medecin }));
       }
-
-
     }
+
+    if (selectedCity) {
+      filteredResults = await Promise.all(
+        filteredResults.map(async (result) => {
+          if (result.cabinet) {
+            const cityName = await getCityName(result.cabinet.latitude, result.cabinet.longitude);
+            return { ...result, city: cityName };
+          }
+          return result;
+        })
+      );
+
+      filteredResults = filteredResults.filter((result) => {
+        if (result.cabinet) {
+          return result.city === selectedCity;
+        }
+        return true;
+      });
+    }
+
     setResults(filteredResults);
+  };
+
+  const getCityName = async (latitude, longitude) => {
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyATeoALYkOOyyAcXF0gqTgZH1zxUgAdpnU`
+      );
+      const data = await response.json();
+      if (data.status === 'OK') {
+        const cityName = data.results[0].address_components.find(
+          (component) =>
+            component.types.includes('locality') ||
+            component.types.includes('sublocality') ||
+            component.types.includes('administrative_area_level_1')
+        ).long_name;
+        return cityName;
+      } else {
+        return 'Aucune ville trouvée';
+      }
+    } catch (error) {
+      console.error('Erreur lors de la conversion :', error);
+      return 'Erreur lors de la conversion';
+    }
   };
 
   return (
     <div className="hero_map">
       <div id="map_listing"></div>
-      <form onSubmit={handleSearch} className="search_wp" >
+      <form onSubmit={handleSearch} className="search_wp">
         <div id="custom-search-input">
           <div className="input-group">
             <input
@@ -85,15 +120,28 @@ function SearchZone() {
                 <label> Specialite </label>
               </button>
             </li>
-
+            <li>
+              <select id='select' value={selectedCity} onChange={(event) => setSelectedCity(event.target.value)}>
+                <option value="">Select a city</option>
+                {cities.map((city) => (
+                  <option key={city} value={city}>
+                    {city}
+                  </option>
+                ))}
+              </select>
+            </li>
           </ul>
           <ul>
             {results.map((result) => (
               <li key={result.doctor ? result.doctor.id : result.cabinet.id}>
-                {result.city && <p>{result.city}</p>}
+                {/* {result.city && <p>{result.city}</p>} */}
                 {result.doctor ? (
                   <div>
-                    {filter === 'Doctor' && <p>{result.doctor.firstName} {result.doctor.lastName}</p>}
+                    {filter === 'Doctor' && (
+                      <p>
+                        {result.doctor.firstName} {result.doctor.lastName}
+                      </p>
+                    )}
                     {filter === 'Specialite' && <p>{result.doctor.specialite}</p>}
                     {filter === 'All' && <p>{result.cabinet.denomination}</p>}
                   </div>
@@ -105,11 +153,10 @@ function SearchZone() {
               </li>
             ))}
           </ul>
-
         </div>
       </form>
     </div>
   );
-}
+};
 
 export default SearchZone;
