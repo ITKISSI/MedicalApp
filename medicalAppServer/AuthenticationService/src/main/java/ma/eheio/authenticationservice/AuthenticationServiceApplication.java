ca4cb68304f535fec3303b26fe53b244e580cf32
Abdelaziz kissi <azizkissi29@gmail.com>
2023-05-24 16:25:40 +0100

    Authentication service user

--- medicalAppServer/AuthenticationService/src/main/java/ma/eheio/authenticationservice/AuthenticationServiceApplication.java
+++ medicalAppServer/AuthenticationService/src/main/java/ma/eheio/authenticationservice/AuthenticationServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class AthenticationServiceApplication {
+public class AuthenticationServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AthenticationServiceApplication.class, args);
+        SpringApplication.run(AuthenticationServiceApplication.class, args);
     }
 
 
--- medicalAppServer/AuthenticationService/src/main/java/ma/eheio/authenticationservice/controller/AuthenticationController.java
+++ medicalAppServer/AuthenticationService/src/main/java/ma/eheio/authenticationservice/controller/AuthenticationController.java
@@ -5,13 +5,11 @@ import ma.eheio.authenticationservice.model.User;
 import ma.eheio.authenticationservice.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Authentication")
+@CrossOrigin("http://localhost:3000")
 public class AuthenticationController {
 
 
